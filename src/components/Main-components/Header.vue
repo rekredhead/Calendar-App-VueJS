73bec479cc151344deceb48e4a5d3643798f2747
rekredhead <rekredhead@gmail.com>
2023-08-28 18:40:24 +0530

    Added conditional rendering for notifications alert element in Header

--- src/components/Main-components/Header.vue
+++ src/components/Main-components/Header.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+const notifications: any[] = [ 1, 2, 3]; // Change the type based on the actual notifications data
 const modes = ['list', 'monthly', 'weekly'];
 const mode = ref(modes[1]);
 
 const toTitleCase = (word: string) => word.toLowerCase().replace(/(?:^|\s)\w/g, match => match.toUpperCase());
-
 const handleClick = (item: string) => {
    if (mode.value === item) return; // Do not run if the mode hasn't changed
 
@@ -29,16 +29,16 @@ const handleClick = (item: string) => {
          </div>
       </div>
       <div class="flex items-center w-1/5 justify-between h-11">
-         <input
-            type="search"
+         <input type="search"
             class="px-5 h-[80%] rounded-full border border-slate-400 outline-none bg-transparent focus:bg-white focus:border-none"
-            placeholder="Search"
-         />
-         <button class="flex items-center justify-center h-full aspect-square bg-blue-600 rounded-full hover:opacity-80 active:opacity-60 transition duration-100">
+            placeholder="Search" />
+         <button
+            class="flex items-center justify-center h-full aspect-square bg-blue-600 rounded-full hover:opacity-80 active:opacity-60 transition duration-100">
             <span class="material-symbols-outlined text-white text-2xl">add</span>
          </button>
          <button class="flex relative p-2 rounded-full bg-white hover:bg-slate-100 active:bg-slate-200">
-            <div class="flex absolute top-0 right-0 bg-red-500 h-4 w-4 rounded-full translate-x-2 -translate-y-1"></div>
+            <div v-if="notifications.length"
+               class="flex absolute top-0 right-0 bg-red-500 h-4 w-4 rounded-full translate-x-1"></div>
             <span class="material-symbols-outlined">notifications</span>
          </button>
       </div>
