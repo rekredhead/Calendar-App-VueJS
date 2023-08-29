23e90d38353e85858d56102559c148d0a1fdedb7
rekredhead <rekredhead@gmail.com>
2023-08-29 20:09:34 +0530

    Added simple weekly calendar for the current week. Alignment styles must be fixed. Main functionality is not implemented yet (task-adding)

--- src/components/Main-components/Body.vue
+++ src/components/Main-components/Body.vue
@@ -1,12 +1,67 @@
 <script setup lang="ts">
-import { eachDayOfInterval, endOfWeek, startOfToday, startOfWeek, format } from 'date-fns';
+import { eachDayOfInterval, endOfWeek, startOfToday, startOfWeek, set, format, isSameDay, eachHourOfInterval } from 'date-fns';
+//import { ref } from 'vue';
+
+/** Things to finish
+ * Align the weekly dates (28 Mon, 29 Tue) properly so it is present even when scrolling
+ * Make the timezones get scrolled along with the calendar body
+*/
 
 const currentDate = startOfToday();
-const currentWeekStart = startOfWeek(currentDate);
-const currentWeekEnd = endOfWeek(currentDate);
+const currentWeekStart = startOfWeek(currentDate, { weekStartsOn: 1 });
+const currentWeekEnd = endOfWeek(currentDate, { weekStartsOn: 1 });
 const daysOfWeek = eachDayOfInterval({ start: currentWeekStart, end: currentWeekEnd });
 
-/*
+const hoursOfDay = eachHourOfInterval({
+   start: currentDate,
+   end: set(currentDate, { hours: 23 })
+});
+
+const convertDecimalTimeTo24HourTime = (decimalTime: number) => {
+   const hours = Math.floor(decimalTime);
+   const decimalMinutes = (decimalTime - hours) * 60;
+   const roundedMinutes = Math.round(decimalMinutes / 15) * 15;
+
+   const time = set(new Date(), { hours, minutes: roundedMinutes });
+   return format(time, 'HH:mm');
+}
+
+const handleDateTimeClick = (e: any) => {
+   const yCoordinate = e.offsetY > 0 ? e.offsetY : 1;
+   const maxHeight = e.target.clientHeight;
+
+   const timeSelected = (yCoordinate / maxHeight) * 12;
+   console.log(convertDecimalTimeTo24HourTime(timeSelected));
+}
+</script>
+
+<template>
+   <main class="flex h-[86%] pl-3 overflow-y-scroll">
+      <div class="w-20 mt-12">
+         <!-- Contains the time-zones like 09:00, 10:00 -->
+         <div v-for="hour in hoursOfDay" class="h-16 text-slate-500 text-center">{{ format(hour, 'HH:mm') }}</div>
+      </div>
+      <div class="flex justify-evenly w-full h-full">
+         <!-- Each day of the week is a column containing the day-label and a clickable area to add tasks -->
+         <div class="flex relative flex-col w-full" v-for="dayOfWeek in daysOfWeek">
+            <div
+               class="flex absolute top-0 right-0 left-0 gap-1 text-slate-500 justify-center p-2 items-center bg-red-500">
+               <p :class="{ 'text-white bg-blue-600': isSameDay(dayOfWeek, currentDate) }"
+                  class="text-center h-full aspect-square text-xl rounded-full p-1">
+                  {{ format(dayOfWeek, 'dd') }}
+               </p>
+               <p :class="{ 'text-blue-600': isSameDay(dayOfWeek, currentDate) }" class="font-bold">{{ format(dayOfWeek,
+                  'EEE') }}</p>
+            </div>
+            <div @click="handleDateTimeClick" class="w-full h-full mt-16">
+               <div v-for="_ in Array(24)" class="h-16 border-l border-b border-slate-400"></div>
+            </div>
+         </div>
+      </div>
+   </main>
+</template>
+
+<!--
 Idea:
 each day of the week (e.g. 23-Mon or 24-Tue) has it's own object in an array. For example:
 [{
@@ -37,20 +92,4 @@ After the time was obtained, it should open the slide-in panel to allow the user
    The slide-in panel should have the startTime as the time that was clicked on.
 The slide-in panel submission should have the correct data as the EXAMPLE ARRAY above.
 Use sample data to render some tasks in different times at different dates.
-*/
-</script>
-
-<template>
-   <main class="h-[86%] overflow-y-scroll">
-      <div class="flex border border-black justify-evenly">
-         <span class="" v-for="dayOfWeek in daysOfWeek">
-            {{ format(dayOfWeek, 'dd EEE') }}
-         </span>
-      </div>
-      Plan how the calendar will be rendered, put events data as well
-      Note the problems - analyse the problem - break them down to smaller problems
-      <div v-for="(_, index) in Array(50)">
-         {{ index }}
-      </div>
-   </main>
-</template>
+-->
