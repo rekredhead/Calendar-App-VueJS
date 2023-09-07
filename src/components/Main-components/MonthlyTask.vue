61725eceaaed2606dded8638dd88a3f20ad03247
rekredhead <rekredhead@gmail.com>
2023-09-07 13:35:11 +0530

    Made monthly calendar appointments editable

--- src/components/Main-components/MonthlyTask.vue
+++ src/components/Main-components/MonthlyTask.vue
@@ -7,10 +7,21 @@ const props = defineProps({
    event: Object as PropType<Event>,
    openSidePanel: Function
 });
+
+const handleClick = () => {
+   props.openSidePanel!({
+      id: props.event!.id,
+      service: props.event!.service,
+      patient: props.event!.patient,
+      startingDateTime: props.event!.startTime,
+      endingDateTime: props.event!.endTime
+   });
+}
 </script>
 
 <template>
-   <div :key="props.event!.id" class="flex bg-blue-500 text-white rounded-md px-2 py-0.5 mb-1 gap-2 overflow-hidden">
+   <div :key="props.event!.id" @click="handleClick"
+      class="flex cursor-pointer bg-blue-500 text-white rounded-md px-2 py-0.5 mb-1 gap-2 overflow-hidden">
       <p>{{ format(props.event!.startTime, 'h:m a').toLocaleLowerCase() }}</p>
       <p>{{ props.event!.service.nameOfService }}</p>
    </div>
